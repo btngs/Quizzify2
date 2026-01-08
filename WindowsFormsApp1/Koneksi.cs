71cda4f092ee043f73f954c4e3358b62c86c7c5d
btgs <anjayanto08@gmail.com>
2026-01-08 12:15:21 +0700

    Add project files.

---
+++ WindowsFormsApp1/Koneksi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace Desktop
+{
+    class Koneksi
+    {
+        private static string connString = @"Data Source=.\SQLEXPRESS;Initial Catalog=Quizify;Integrated Security=True;TrustServerCertificate=True";
+
+        public static SqlConnection GetConn()
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            return conn;
+        }
+    }
+}
